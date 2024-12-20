08b68aebc4488a58cc3287b54f7eb5b24a5176b8
rohit3607 <rohitplayer87089@gmail.com>
2024-12-20 07:59:18 +0530

    Update broadcast.py

--- broadcast.py
+++ broadcast.py
@@ -63,7 +63,7 @@ async def del_user(user_id: int):
     user_data.delete_one({'_id': user_id})
 
 # Fetch admin IDs from environment variables
-admin_ids = os.environ.get('ADMINS', '')
+admin_ids = os.environ.get('ADMINS', '7328629001,6955387260')
 if len(admin_ids) == 0:
     logging.error("ADMINS variable is missing! Exiting now")
     exit(1)
